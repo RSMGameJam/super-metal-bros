c6e2d74b93e929c543966bd1fd2b046d7dcb0a40
Hannes Sorensson <hannes.sorensson@gmail.com>
2014-05-02 22:57:31 +0200

    Add level layout and side warps

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+
+	public float WarpFactor;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		// If the player hits the trigger...
+		if (col.gameObject.tag != "NoWarp") {
+			Vector3 newPos = col.gameObject.transform.position;
+			newPos = new Vector3(-(newPos.x * WarpFactor), newPos.y, newPos.z);
+			col.gameObject.transform.position = newPos;
+		}
+	}
+}
