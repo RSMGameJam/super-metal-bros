a8995baf072e1345767de076e63715ff3917f46d
unknown <Edebarudo@Ultimatronic.(none)>
2014-05-03 08:30:21 +0200

    Score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,34 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Ebbe.Cereal;
 
 public class Score : MonoBehaviour
 {
-	public int score = 0;					// The player's score.
+	public float countUpDuration = 1f;
 
+	[HideInInspector]public int score = 0;	// The player's score.
 
 	private PlayerControl playerControl;	// Reference to the player control script.
 	private int previousScore = 0;			// The score in the previous frame.
 
+	private Cereal _cereal = new Cereal();
+
+	public void AddScore(int pAddScore) {
+		score += pAddScore;
+		_cereal.Clear();
+		_cereal.Add(new CerealLerpEvent<float>(previousScore, (float)score, countUpDuration, Mathf.Lerp)
+		{
+			OnChange = (pChange) => {
+				guiText.text = "Score: " + (int)pChange;
+			}
+		});
+		previousScore = score;
+	}
 
 	void Awake ()
 	{
 		// Setting up the reference.
 		playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
-	}
 
+		// Set empty score
+		guiText.text = "Score: " + score;
+	}
 
 	void Update ()
 	{
-		// Set the score text.
-		guiText.text = "Score: " + score;
-
-		// If the score has changed...
-		if(previousScore != score)
-			// ... play a taunt.
-			playerControl.StartCoroutine(playerControl.Taunt());
-
-		// Set the previous score to this frame's score.
-		previousScore = score;
+		_cereal.Update();
 	}
-
 }
