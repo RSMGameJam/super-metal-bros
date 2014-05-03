5c7f2a944a53f7253f35c51042978c59f8c34351
Edvard Rutstrom <liquidcrate@gmail.com>
2014-05-03 15:34:48 +0200

    states

---
+++ Assets/StateHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState {
+	STARTMENU,
+	LEVELONE
+}
+
+public class StateHandler : MonoBehaviour {
+
+	public static StateHandler current;
+
+	public const string START_MENU = "StartMenu";
+	public const string LEVEL_ONE = "Level01";
+
+	static GameState _state;
+	public static GameState state {
+		get {
+			return _state;
+		}
+		set {
+			switch(value) {
+				case GameState.STARTMENU:
+					//if(Application.loadedLevelName != START_MENU)
+						Application.LoadLevel(START_MENU);
+					break;
+				case GameState.LEVELONE:
+					//if(Application.loadedLevelName != LEVEL_ONE)
+						Application.LoadLevel(LEVEL_ONE);
+					break;
+			}
+		}
+	}
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+
+		current = this;
+
+		// Start menu
+		state = GameState.STARTMENU;
+	}
+}
