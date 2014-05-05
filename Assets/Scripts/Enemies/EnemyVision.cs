3d3b781eb0dea3c3168fae45c9bed59e0bdc3c99
Edebarudo <Liquidcrate@gmail.com>
2014-05-05 23:37:57 +0200

    Fixed the raycasting

--- Assets/Scripts/Enemies/EnemyVision.cs
+++ Assets/Scripts/Enemies/EnemyVision.cs
@@ -22,12 +22,11 @@ public class EnemyVision : MonoBehaviour
 		// raycasting
 		float facing = (transform.localScale.x < 0f ? -1f : 1f);
 		Vector2 origin = (Vector2)eye.position + offset*facing;
-		Vector2 direction = facing*Vector2.right * transform.localScale.x;
+		Vector2 direction = Vector2.right * transform.localScale.x;
 		Ray ray = new Ray(origin, direction);
 
 		RaycastHit2D hit;
 		hit = Physics2D.Raycast(ray.origin, ray.direction, visionDistance);
-		//Debug.DrawRay(ray.origin, ray.direction  * visionDistance, Color.yellow);
 		Debug.DrawRay(ray.origin, ray.direction.normalized * visionDistance, Color.yellow);
 
 		if(hit != null && hit.collider != null) {
@@ -39,28 +38,6 @@ public class EnemyVision : MonoBehaviour
 					player.Die();
 			}
 		}
-
-		//Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-		//RaycastHit2D hit = Physics2D.Raycast((Vector2)this.transform.position + offset, rotation * (Vector2.right * transform.localScale.x), visionDistance);
-		//Debug.DrawRay((Vector2)this.transform.position + offset, rotation * (Vector2.right * transform.localScale.x * visionDistance), Color.yellow);
-
-		//if(hit != null && hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
-		//{
-		//	Player player = hit.collider.GetComponent<Player>();
-		//	if(player != null)
-		//		player.Die();
-		//}
-
-		//rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
-		//hit = Physics2D.Raycast((Vector2)this.transform.position + offset, rotation * (Vector2.right * transform.localScale.x + offset));
-		//Debug.DrawRay((Vector2)this.transform.position + offset, rotation * (Vector2.right * transform.localScale.x * visionDistance ), Color.yellow);
-
-		//if(hit != null && hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
-		//{
-		//	Player player = hit.collider.GetComponent<Player>();
-		//	if(player != null)
-		//		player.Die();
-		//}
     }
 
 	void OnDrawGizmos()
